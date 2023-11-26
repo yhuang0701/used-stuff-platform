9c1d840359cf35866197c4746b68205a51312edd
peterzhu <z18819828123@gmail.com>
2023-11-26 09:36:45 +0800

    Web header init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import React from 'react';
+import Header from './components/Header';
+
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    return (
+        <>
+            <Header />
+            <main>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+                <Routes>
+                    {/* Add additional routes for other pages */}
+                </Routes>
+            </main>
+        </>
+    );
+};
 
 export default App;
