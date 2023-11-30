9dd63a828fe0c01eafae32af68980812097635bc
Loriasun <loriasun@umich.edu>
2023-11-30 12:36:10 -0600

    Updated schema files

---
+++ backend/models/UserInfo.js
@@ -0,0 +1,34 @@
+// Assuming auth.js is in the same directory
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./Auth.js'); // Import User model
+const Item = require('./UserInfo.js')
+
+const UerInfoSchema = new Schema({
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    items: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Item'
+    }]
+});
+
+const UserInfo = mongoose.model('ItemInfo', UerInfoSchema);
+
+module.exports = UserInfo;
