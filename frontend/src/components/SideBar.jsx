ba3de72c5d1babde9d00ecfca3bdafe363ad6fa8
Yichen Huang <yichen41@illinois.edu>
2023-12-13 04:16:42 +0800

    Updated user profile with real data and logout auth

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -8,14 +8,22 @@ import {
   MdContacts,
   MdPowerSettingsNew
 } from 'react-icons/md';
+import { useAuth } from '../View/AuthContext';
 
 const SideBar = () => {
+
+  const auth = useAuth()
+
+  const handleSignOut = () => {
+    auth.logout();
+  };
+
   return (
     <div className="bg-white text-gray-700 min-h-screen px-20 py-10">
       <div className="font-bold text-xl mb-8 pl-6 pt-6">Sidebar</div>
       <nav>
         <NavLink to="/dashboard" className="flex items-center p-4 hover:bg-gray-100">
-          <MdDashboard className="mr-4" /> Dashboard
+          <MdDashboard className="mr-4" /> View My Posted Items
         </NavLink>
         <NavLink to="/profile" className="flex items-center p-4 hover:bg-gray-100">
           <MdPerson className="mr-4" /> Profile
@@ -29,7 +37,7 @@ const SideBar = () => {
         <NavLink to="/contact" className="flex items-center p-4 hover:bg-gray-100">
           <MdContacts className="mr-4" /> Change Contact Info
         </NavLink>
-        <NavLink to="/logout" className="flex items-center p-4 hover:bg-gray-100">
+        <NavLink to="/home" onClick={handleSignOut} className="flex items-center p-4 hover:bg-gray-100">
           <MdPowerSettingsNew className="mr-4" /> Log Out
         </NavLink>
       </nav>
