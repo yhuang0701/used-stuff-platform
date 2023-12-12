9e4b7f52104559f7ee8ee4964e746e2d95716e4f
Yichen Huang <yichen41@illinois.edu>
2023-12-13 04:27:25 +0800

    Updated navigation to user detail page with user id

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -12,6 +12,7 @@ import { useAuth } from '../View/AuthContext';
 
 const SideBar = () => {
 
+  const userId = localStorage.getItem('userID');
   const auth = useAuth()
 
   const handleSignOut = () => {
@@ -19,24 +20,24 @@ const SideBar = () => {
   };
 
   return (
-    <div className="bg-white text-gray-700 min-h-screen px-20 py-10">
-      <div className="font-bold text-xl mb-8 pl-6 pt-6">Sidebar</div>
+    <div className="bg-white text-gray-700 min-h-screen px-20 py-20">
+      <div className="font-bold text-xl mb-8 pl-6 pt-6">Dashboard</div>
       <nav>
-        <NavLink to="/dashboard" className="flex items-center p-4 hover:bg-gray-100">
-          <MdDashboard className="mr-4" /> View My Posted Items
-        </NavLink>
         <NavLink to="/profile" className="flex items-center p-4 hover:bg-gray-100">
           <MdPerson className="mr-4" /> Profile
         </NavLink>
-        <NavLink to="/settings" className="flex items-center p-4 hover:bg-gray-100">
-          <MdSettings className="mr-4" /> Settings
+        <NavLink to="/UserDetail" state={{ userId: userId }} className="flex items-center p-4 hover:bg-gray-100">
+          <MdDashboard className="mr-4" /> View My Posted Items
         </NavLink>
-        <NavLink to="/change-password" className="flex items-center p-4 hover:bg-gray-100">
+        {/* <NavLink to="/settings" className="flex items-center p-4 hover:bg-gray-100">
+          <MdSettings className="mr-4" /> Settings
+        </NavLink> */}
+        {/* <NavLink to="/change-password" className="flex items-center p-4 hover:bg-gray-100">
           <MdVpnKey className="mr-4" /> Change Password
         </NavLink>
         <NavLink to="/contact" className="flex items-center p-4 hover:bg-gray-100">
           <MdContacts className="mr-4" /> Change Contact Info
-        </NavLink>
+        </NavLink> */}
         <NavLink to="/home" onClick={handleSignOut} className="flex items-center p-4 hover:bg-gray-100">
           <MdPowerSettingsNew className="mr-4" /> Log Out
         </NavLink>
